bea1ca23ad899183cb1b2d6a02d8bb49f665d333
Ryan Fehr <coderry4@gmail.com>
2025-08-14 19:36:48 -0500

    Add Twitter API consumer and access token keys

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
 import tweepy
 from textblob import TextBlob
 
+consumer_key = 'rczZDXlAKoWfepiJIjAiB6sLx'
+consumer_secret = '4GdjNhUYyBdyIipIgkFJxcFMK1GC05cebYO17nHQnkViUKEFcJ'
+
+access_token = '1535726073987616768-m8O6o9ffOXnQM6y5Oem9z4j8nFM7a2'
+access_secret = 'f5PL9W6dR3Mjyo3fHkybNTTq9GsSvMsBPiXhUno7bLfTk'
+
