26b7bfb412e6c3cd89f8d4e53383a969ebc07a39
Ryan Fehr <coderry4@gmail.com>
2025-08-14 19:46:01 -0500

    Add API method

--- src/main.py
+++ src/main.py
@@ -8,5 +8,7 @@ access_token = '1535726073987616768-m8O6o9ffOXnQM6y5Oem9z4j8nFM7a2'
 access_secret = 'f5PL9W6dR3Mjyo3fHkybNTTq9GsSvMsBPiXhUno7bLfTk'
 
 auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret)
-
 auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
