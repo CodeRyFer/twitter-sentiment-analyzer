42e7ea9510b4ad7e2129d14bc0d7e456b6b5b29c
Ryan Fehr <coderry4@gmail.com>
2025-08-14 19:44:24 -0500

    Fix authentication handler

--- src/main.py
+++ src/main.py
@@ -7,4 +7,6 @@ consumer_secret = '4GdjNhUYyBdyIipIgkFJxcFMK1GC05cebYO17nHQnkViUKEFcJ'
 access_token = '1535726073987616768-m8O6o9ffOXnQM6y5Oem9z4j8nFM7a2'
 access_secret = 'f5PL9W6dR3Mjyo3fHkybNTTq9GsSvMsBPiXhUno7bLfTk'
 
-auth = tweepy.OAuth2AppHandler(consumer_key, consumer_secret)
+auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret)
+
+auth.set_access_token(access_token, access_secret)
